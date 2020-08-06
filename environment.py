8e2eed77e03ca38864d48a025720ee771717eb3f
charlie-kun <chlh@daum.net>
2020-08-06 14:23:51 +0900

    add notes

--- environment.py
+++ environment.py
@@ -1,23 +1,24 @@
 class Environment:
-    PRICE_IDX = 4  # 종가의 위치
+    PRICE_IDX = 4  # End Value's position
 
-    def __init__(self, chart_data=None):
+    def __init__(self, chart_data=None):        #select chart_data
+        #__init__ is automatically made mathod.
         self.chart_data = chart_data
         self.observation = None
-        self.idx = -1
+        self.idx = -1       #Present position
 
-    def reset(self):
+    def reset(self):        #Move to first postion in data
         self.observation = None
         self.idx = -1
 
-    def observe(self):
-        if len(self.chart_data) > self.idx + 1:
+    def observe(self):          #Move to yesterday data and display
+        if len(self.chart_data) > self.idx + 1:     #len() is data length.
             self.idx += 1
-            self.observation = self.chart_data.iloc[self.idx]
+            self.observation = self.chart_data.iloc[self.idx]       #"iloc method" is "dataframe method". and they are do pickup to line data.
             return self.observation
         return None
 
-    def get_price(self):
+    def get_price(self):        #observe data and get end price return.
         if self.observation is not None:
             return self.observation[self.PRICE_IDX]
         return None
